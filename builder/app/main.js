bcf031863157a81464ae3c21e0103e7ccb0373ea
jasonh <appleoxxo@gmail.com>
2022-03-24 16:26:36 +0800

    fix wrong path on production

--- builder/app/main.js
+++ builder/app/main.js
@@ -1,22 +1,26 @@
-const { app, BrowserWindow, protocol, remote } = require('electron');
+const { app, BrowserWindow, protocol } = require('electron');
 const path = require('path');
 const url = require('url');
 
-const IS_PRODUCTION = app.isPackaged;
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const CRA_BUILD_FOLDER = `cra-build`;
 
 let window;
 
 function createWindow() {
   window = new BrowserWindow({ width: 800, height: 600 });
 
-  const appUrl = IS_PRODUCTION
-    ? url.format({
-        pathname: path.join(remote.app.getAppPath(), 'cra-build', 'index.html'),
-        protocol: 'app',
-        slashes: true,
-      })
-    : 'http://localhost:3000';
+  const appUrl = url.format({
+    pathname: 'index.html',
+    protocol: 'file',
+    slashes: true,
+  });
 
+  /**
+   * if need HMR while developing cra
+   * const appUrl = 'http://localhost:3000';
+   *
+   */
   console.log(`$ appUrl ${appUrl}`);
   window.loadURL(appUrl);
 
@@ -27,11 +31,22 @@ function createWindow() {
 }
 
 app.on('ready', () => {
-  protocol.registerFileProtocol(
-    'app',
+  protocol.interceptFileProtocol(
+    'file',
     (request, callback) => {
       const url = request.url.substr(7); /* all urls start with 'file://' */
-      callback({ path: path.normalize(`${__dirname}/${url}`) });
+      if (IS_PRODUCTION) {
+        callback({
+          path: path.normalize(
+            path.join(app.getAppPath(), CRA_BUILD_FOLDER, url),
+          ),
+        });
+        return;
+      }
+
+      callback({
+        path: path.normalize(path.join(__dirname, CRA_BUILD_FOLDER, url)),
+      });
     },
     (err) => {
       if (err) console.error('Failed to register protocol');
